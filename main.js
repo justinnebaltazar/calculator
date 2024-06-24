57f310d59473b822d3673a43b73f52d57ba7ce44
justinnebaltazar <jfb3@sfu.ca>
2024-06-24 16:50:45 -0700

    Add a function for each basic operation

---
+++ main.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    let sum = a + b; 
+    return sum;
+}
+
+function subtract(a, b) {
+    let difference = a - b; 
+    return difference; 
+}
+
+function multiply(a, b) {
+    let product = a * b; 
+    return product;
+}
+
+function divide(a, b) {
+    let quotient = a / b; 
+    return quotient;
+}
+
+console.log(add(4, 9)); 
+console.log(subtract(4, 9)); 
+console.log(multiply(4, 9)); 
+console.log(divide(36, 9)); 
