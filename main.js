ad61793ded4b19c8b9664d3900015fe33dff8d42
justinnebaltazar <jfb3@sfu.ca>
2024-06-24 17:05:05 -0700

    Add buttons for numbers and operators

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 function add(a, b) {
     let sum = a + b; 
     return sum;
-}
+};
 
 function subtract(a, b) {
     let difference = a - b; 
     return difference; 
-}
+};
 
 function multiply(a, b) {
     let product = a * b; 
     return product;
-}
+};
 
 function divide(a, b) {
     let quotient = a / b; 
     return quotient;
-}
+};
 
-console.log(add(4, 9)); 
-console.log(subtract(4, 9)); 
-console.log(multiply(4, 9)); 
-console.log(divide(36, 9)); 
+function operate(operator, a, b) {
+    if (operator.toLowerCase() === 'add') {
+        add(a, b);
+    } else if (operator.toLowerCase() === 'subtract') {
+        subtract(a, b); 
+    } else if (operator.toLowerCase() === 'multiply') {
+        multiply(a, b);
+    } else if (operator.toLowerCase() === 'divide') {
+        divide(a, b);
+    }
+};
